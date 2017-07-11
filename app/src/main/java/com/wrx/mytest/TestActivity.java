d9d30c4120fa31f582f7f851cedbe91d6a6760dc
wrx <wowuruixiong@163.com>
2017-07-11 18:06:17 +0800

    change test acitvity

--- app/src/main/java/com/wrx/mytest/TestActivity.java
+++ app/src/main/java/com/wrx/mytest/TestActivity.java
@@ -1,7 +1,10 @@
 package com.wrx.mytest;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
@@ -16,6 +19,41 @@ public class TestActivity extends Activity{
         TextView textView = new TextView(this);
         textView.setText("Hello1");
         setContentView(textView);
+        String a = getDeviceName();
+        Log.e("", "");
+    }
+
+
+    /** Returns the consumer friendly device name */
+    public static String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        }
+        return capitalize(manufacturer) + " " + model;
+    }
+
+    private static String capitalize(String str) {
+        if (TextUtils.isEmpty(str)) {
+            return str;
+        }
+        char[] arr = str.toCharArray();
+        boolean capitalizeNext = true;
+
+        StringBuilder phrase = new StringBuilder();
+        for (char c : arr) {
+            if (capitalizeNext && Character.isLetter(c)) {
+                phrase.append(Character.toUpperCase(c));
+                capitalizeNext = false;
+                continue;
+            } else if (Character.isWhitespace(c)) {
+                capitalizeNext = true;
+            }
+            phrase.append(c);
+        }
+
+        return phrase.toString();
     }
 
 }
